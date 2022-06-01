ee1d600683a00cf79f042c297b1435d6cec7e628
Mayu Suzumura <mayumurasuzu@gmail.com>
2022-06-01 15:09:49 -0400

    Created routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import './App.css';
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+
+import "./App.css";
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
+import Work from "./components/pages/Work";
+import Skill from "./components/pages/Skill";
+import Contact from "./components/pages/Contact";
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Link to="/">home</Link>
+        <Link to="/about">about</Link>
+        <Link to="/work">work</Link>
+        <Link to="/skill">skill</Link>
+        <Link to="/contact">contact</Link>
+      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/work" element={<Work />} />
+        <Route path="/skill" element={<Skill />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
