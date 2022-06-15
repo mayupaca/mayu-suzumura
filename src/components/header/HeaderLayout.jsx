293b2c5e4f21aa3eec9c8a87bc5084ba4afa8e08
Mayu Suzumura <mayumurasuzu@gmail.com>
2022-06-14 21:11:15 -0400

    finished header refactoring

--- src/components/header/HeaderLayout.jsx
+++ src/components/header/HeaderLayout.jsx
@@ -3,11 +3,25 @@ import Header from "./Header";
 
 const HeaderLayout = () => {
   const headerPages = ["home", "about", "work", "skill", "contact"];
+
   return (
     <div class="sticky top-0 flex justify-end h-20 text-md pt-10 text-center bg-[#292929]">
       {headerPages.map((headerPage) => (
-        <Header>{headerPage}</Header>
+        <Header headerPage={headerPage}>{headerPage}</Header>
       ))}
+
+      <div
+        className="resume"
+        class="w-24 h-8 mr-10 text-[#FF5757] border border-[#FF5757] rounded-md"
+      >
+        <a
+          href="https://drive.google.com/file/d/114evEJ_JhptvgWkq5vlRsa76ry_yElOW/view?usp=sharing"
+          class="block"
+        >
+          resume
+        </a>
+      </div>
+      
     </div>
   );
 };
