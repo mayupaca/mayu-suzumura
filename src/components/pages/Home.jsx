ad2ccad3771ee70fa601f9b4a61c3f4eb5577b26
Mayu Suzumura <mayumurasuzu@gmail.com>
2022-06-24 14:22:49 -0400

    changed home layout

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import "../../style/neon.css";
-import bgImage from "../../images/code_pic.jpg";
+import profilePic from "../../images/mayu_raccoon.JPG";
 
 const Home = () => {
   return (
     <div name="home" class="w-full h-screen">
       <div class="max-w-7xl flex flex-col justify-center h-full">
         <div class="flex flex-row justify-between relative">
-          <img src={bgImage} alt="homePic" class="opacity-50 w-full"></img>
+          <img
+            src={profilePic}
+            alt="image"
+            class="opacity-70 w-[30rem] ml-[30rem] rounded-l-[50%] shadow-2xl shadow-green-500/50"
+          ></img>
           <div class="absolute top-48 right-0 md:right-[-12rem]">
             <div className="greeting" class="text-7xl">
               <h1 class="mb-3 flex justify-end">Hello World!</h1>
