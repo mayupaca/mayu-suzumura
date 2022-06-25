2867891c0537be2ecc884620265c04a08d5e5b9e
Mayu Suzumura <mayumurasuzu@gmail.com>
2022-06-25 12:37:07 -0400

    added hamburger menu

---
+++ src/components/header/HamburgerHeader.jsx
@@ -0,0 +1,24 @@
+import { Link } from "react-scroll";
+
+const HamburgerHeader = (props) => {
+  const { headerPage, selectedPage, setSelectedPage } = props;
+  const redText = () => {
+    setSelectedPage(headerPage);
+  };
+
+  return (
+    <div class="p-5 cursor-pointer">
+      <Link
+        className={headerPage === selectedPage ? "neon-red text-[#FF5757]" : ""}
+        onClick={redText}
+        to={headerPage}
+        smooth={true}
+        duration={500}
+      >
+        {headerPage}
+      </Link>
+    </div>
+  );
+};
+
+export default HamburgerHeader;
--- src/components/header/HeaderLayout.jsx
+++ src/components/header/HeaderLayout.jsx
@@ -1,14 +1,22 @@
 import React, { useState } from "react";
 import Header from "./Header";
 import "../../style/neon.css";
+import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
+import HamburgerHeader from "./HamburgerHeader";
 
 const HeaderLayout = () => {
   const headerPages = ["home", "about", "skill", "work", "contact"];
   //usestate初期値 0 = index0
   const [selectedPage, setSelectedPage] = useState("home");
+  const [hamburger, setHamburger] = useState(false);
+
+  const handleBurger = () => {
+    setHamburger(!hamburger);
+  };
+
   return (
-    <div className="header fixed z-[9999]">
-      <div class="fixed w-full h-[80px] flex flex-row  justify-end items-center px-4 bg-[#292929] text-[#a6a6a6]">
+    <div className="flex justify-end items-center h-24 max-w-[1240px] mx-auto px-4 text-white">
+      <ul className="hidden md:flex">
         {headerPages.map((headerPage) => (
           <Header
             headerPage={headerPage}
@@ -20,12 +28,39 @@ const HeaderLayout = () => {
         ))}
         <div
           className="resume"
-          class="neon-green items-center text-[#FF5757]"
+          class="neon-green items-center text-[#FF5757] mr-10"
         >
           <a href="#">resume</a>
         </div>
+      </ul>
+      <div onClick={handleBurger} className="block md:hidden">
+        {hamburger ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
       </div>
+      <ul
+        className={
+          hamburger
+            ? "fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-500 bg-[#6c6f6c] ease-in-out duration-500"
+            : "ease-in-out duration-500 fixed left-[-100%]"
+        }
+      >
+        {headerPages.map((headerPage) => (
+          <HamburgerHeader
+            headerPage={headerPage}
+            setSelectedPage={setSelectedPage}
+            selectedPage={selectedPage}
+          >
+            {headerPage}
+          </HamburgerHeader>
+        ))}
+        <div
+          className="resume"
+          class="neon-green items-center text-[#FF5757] p-5"
+        >
+          <a href="#">resume</a>
+        </div>
+      </ul>
     </div>
+    
   );
 };
 
