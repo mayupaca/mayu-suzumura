760748f372080ec8ca9502a7acf6fdd278a70b52
Mayu Suzumura <mayumurasuzu@gmail.com>
2022-06-27 09:29:12 -0400

    home page responsive

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -7,25 +7,32 @@ const Home = () => {
     <div name="home" class="w-full h-screen">
       <div class="max-w-7xl m-auto flex flex-col justify-center h-full">
         <div class="justify-between relative">
-          <div class="absolute top-48 left-96">
-            <div className="greeting" class="text-7xl">
-              <h1 class="mb-3 flex justify-end">Hello World!</h1>
-              <h1 class="mb-3 flex justify-end">
+          <div class="md:absolute top-48 left-96 z-10">
+            <div className="greeting" class="text-center text-4xl md:text-7xl">
+              <h1 class="mb-3 md:flex justify-end">Hello World!</h1>
+              <h1 class="mb-3 md:flex justify-end">
                 <span class="neon-yellow">I&nbsp;</span>am&nbsp;
               </h1>
-              <h1 class="mb-3 flex justify-end neon-yellow">
-                Mayu Suzumura.
+              <h1 class="mb-3 inline md:flex justify-end">
+                <span class="neon-yellow">Mayu&nbsp;Suzumura</span>.
               </h1>
             </div>
-            <div className="intro" class="text-5xl mt-5">
-              <h2 class="mb-3 flex justify-end">I am from<span class="neon-red">&nbsp;Japan</span>.</h2>
+            <div
+              className="intro"
+              class="text-center text-3xl md:text-5xl md:flex justify-end mt-5 "
+            >
+              <h2 class="mb-3 inline md:justify-end">
+                I am from<span class="neon-red">&nbsp;Japan</span>.
+              </h2>
             </div>
           </div>
-          <img
-            src={profilePic}
-            alt="me"
-            class="w-[30rem] rounded-l-[50%] shadow-2xl shadow-green-500/50 opacity-70 "
-          ></img>
+          <div class="flex justify-center md:justify-start h-[250px] mt-5">
+            <img
+              src={profilePic}
+              alt="me"
+              class="w-[10rem] md:w-[30rem] h-[13rem] md:h-[40rem] md:rounded-l-[50%] shadow-2xl shadow-green-500/50 opacity-70 "
+            ></img>
+          </div>
         </div>
       </div>
     </div>
